cf6da253d39856811bd846bf0c7b2886f60cee62
Tonukung <69027553+PatcharaKongaum@users.noreply.github.com>
2023-11-05 02:40:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import tkinter as tk
 from gtts import gTTS
 import os
 
-# กำหนดช่วงสีแต่ละสีในรูปแบบ HSV
 blue_lower = (90, 50, 50)
 blue_upper = (130, 255, 255)
 
@@ -21,7 +20,6 @@ yellow_upper = (35, 255, 255)
 green_lower = (35, 50, 50)
 green_upper = (85, 255, 255)
 
-# สร้างฟังก์ชันสำหรับแสดงผลลัพธ์สี
 def display_color_results(green_pixel_count, blue_pixel_count, purple_pixel_count, red_pixel_count, yellow_pixel_count):
     root = tk.Tk()
     root.title("Results")
@@ -33,30 +31,21 @@ def display_color_results(green_pixel_count, blue_pixel_count, purple_pixel_coun
 
     root.mainloop()
 
-# สร้างฟังก์ชันสำหรับถ่ายรูปและตรวจสอบสี
 def capture_and_detect_color():
-    # เปิดกล้อง
     cap = cv2.VideoCapture(0)
 
-    # ถ่ายรูปภาพ
     ret, frame = cap.read()
     cv2.imwrite("captured_image.jpg", frame)
 
-    # ปิดกล้อง
     cap.release()
 
-    # ตรวจสอบสีจากรูปภาพที่ถ่าย
     detect_color_from_image('captured_image.jpg')
 
-# สร้างฟังก์ชันสำหรับตรวจสอบสีจากรูปภาพ
 def detect_color_from_image(image_path):
-    # โหลดภาพ
     image = cv2.imread(image_path)
 
-    # แปลงภาพเป็นรูปแบบสี HSV
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # สร้างมาส์สำหรับแต่ละสี
     mask_blue = cv2.inRange(hsv_image, blue_lower, blue_upper)
     mask_purple = cv2.inRange(hsv_image, purple_lower, purple_upper)
     mask_red1 = cv2.inRange(hsv_image, red_lower1, red_upper1)
@@ -65,14 +54,12 @@ def detect_color_from_image(image_path):
     mask_yellow = cv2.inRange(hsv_image, yellow_lower, yellow_upper)
     mask_green = cv2.inRange(hsv_image, green_lower, green_upper)
 
-    # นับจำนวนพิกเซลสีในแต่ละสี
     blue_pixel_count = cv2.countNonZero(mask_blue)
     purple_pixel_count = cv2.countNonZero(mask_purple)
     red_pixel_count = cv2.countNonZero(mask_red)
     yellow_pixel_count = cv2.countNonZero(mask_yellow)
     green_pixel_count = cv2.countNonZero(mask_green)
 
-    # กำหนดเงื่อนไขและสร้างเสียง
     threshold = 1000
     if green_pixel_count >= threshold:
         text_to_speak = "20 บาท"
@@ -93,10 +80,8 @@ def detect_color_from_image(image_path):
         print("Error : Validation conditions not found.")
         text_to_speak = "Error : Validation conditions not found. ไม่พบรูปที่ตรงกับเงื่อนไข"
 
-    # แสดงผลลัพธ์สีในหน้าต่าง UI
     display_color_results(green_pixel_count, blue_pixel_count, purple_pixel_count, red_pixel_count, yellow_pixel_count)
 
-    # สร้างเสียงจากข้อความ
     tts = gTTS(text=text_to_speak, lang='th')
     tts.save("output.mp3")
     os.system("start output.mp3")
